f1c2297e57abf1fdfc36fd1795873835cafca4b6
segur <segur.opus@gmail.com>
2018-12-07 21:04:06 +0900

    Add main flow to call FatnessCheckerDi

--- src/main/java/com/segur/fatness/FatnessCheckerApplication.java
+++ src/main/java/com/segur/fatness/FatnessCheckerApplication.java
@@ -12,7 +12,11 @@ public class FatnessCheckerApplication {
 	 */
 	public static void main(
 			String[] args) {
-
+		
+		// ------------------------------------------------------
+		// BMIを答えてくれる人を肥満度を答えてくれる人が用意する
+		// ------------------------------------------------------
+		
 		// 肥満度を答えてくれる人を呼ぶ
 		FatnessChecker fatnessChecker = new FatnessChecker();
 
@@ -20,6 +24,22 @@ public class FatnessCheckerApplication {
 		String result = fatnessChecker.check(170.0, 70.0);
 
 		// 肥満度を表示する
-		System.out.println(result);
+		System.out.println("内部: " + result);
+		
+		// ------------------------------------------------------
+		// BMIを答えてくれる人をユーザーが用意する
+		// ------------------------------------------------------
+
+		// BMIを答えてくれる人を呼ぶ
+		BmiCalculator bmiCalculator = new BmiCalculator();
+
+		// 肥満度を答えてくれる人を呼ぶ
+		FatnessCheckerDi fatnessCheckerDi = new FatnessCheckerDi(bmiCalculator);
+
+		// 肥満度を答えてもらう
+		String resultDi = fatnessCheckerDi.check(170.0, 70.0);
+
+		// 肥満度を表示する
+		System.out.println("外部: " + resultDi);
 	}
 }
