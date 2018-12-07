ad83b9031de0bf95186407ffbf0e214fe298d660
segur <segur.opus@gmail.com>
2018-12-07 20:49:29 +0900

    Add main flow to call FatnessChecker

--- src/main/java/com/segur/fatness/FatnessCheckerApplication.java
+++ src/main/java/com/segur/fatness/FatnessCheckerApplication.java
@@ -12,5 +12,14 @@ public class FatnessCheckerApplication {
 	 */
 	public static void main(
 			String[] args) {
+
+		// 肥満度を答えてくれる人を呼ぶ
+		FatnessChecker fatnessChecker = new FatnessChecker();
+
+		// 肥満度を答えてもらう
+		String result = fatnessChecker.check(170.0, 70.0);
+
+		// 肥満度を表示する
+		System.out.println(result);
 	}
 }
