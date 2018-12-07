e01be8e14e5da7cc862bc30e355a416446cb1fda
segur <segur.opus@gmail.com>
2018-12-07 20:17:21 +0900

    Create Spring Boot Project

---
+++ src/main/java/com/segur/fatness/FatnessCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.segur.fatness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatnessCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatnessCheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/segur/fatness/FatnessCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.segur.fatness;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FatnessCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
