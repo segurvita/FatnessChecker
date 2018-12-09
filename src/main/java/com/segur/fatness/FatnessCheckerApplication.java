55559eb151a36227adcd68f1d087c205e1bdfb1f
segur <segur.opus@gmail.com>
2018-12-09 15:52:31 +0900

    Change comment

--- src/main/java/com/segur/fatness/FatnessCheckerApplication.java
+++ src/main/java/com/segur/fatness/FatnessCheckerApplication.java
@@ -10,36 +10,35 @@ public class FatnessCheckerApplication {
 	 * 
 	 * @param args
 	 */
-	public static void main(
-			String[] args) {
-		
+	public static void main(String[] args) {
+
 		// ------------------------------------------------------
-		// 肥満度検査会社（外部会社）に作業を依頼する。
+		// 肥満度検査会社（DI導入前）に作業を依頼する。
 		// ------------------------------------------------------
-		
-		// 肥満度検査会社（外部会社）と取引を始める。
+
+		// 肥満度検査会社の人と会話を始める。
 		FatnessChecker fatnessChecker = new FatnessChecker();
 
-		// 肥満度の検査を依頼する。
+		// 身長と体重を伝え、肥満度の判定を依頼する。
 		String result = fatnessChecker.check(170.0, 70.0);
 
-		// 肥満度の検査結果を表示する。
-		System.out.println("肥満度検査結果（外部会社）：" + result);
-		
+		// 肥満度の判定結果を表示する。
+		System.out.println("肥満度検査結果（DI導入前）：" + result);
+
 		// ------------------------------------------------------
-		// 肥満度検査会社（グループ会社）に作業を依頼する。
+		// 肥満度検査会社（DI導入後）に作業を依頼する。
 		// ------------------------------------------------------
 
-		// 自社のBMIマスターを一人確保する。
-		BmiCalculator bmiCalculator = new BmiCalculator();
+		// BMIマスターを１人確保する。
+		BmiMaster bmiCalculator = new BmiMaster();
 
-		// 肥満度検査会社（グループ会社）に自社のBMIマスターを出向させる。
+		// 肥満度検査会社の人に、BMI計算をBMIマスターにしてもらうよう依頼する。
 		FatnessCheckerDi fatnessCheckerDi = new FatnessCheckerDi(bmiCalculator);
 
-		// 肥満度の検査を依頼する。
+		// 身長と体重を伝え、肥満度の判定を依頼する。
 		String resultDi = fatnessCheckerDi.check(170.0, 70.0);
 
 		// 肥満度の検査結果を表示する。
-		System.out.println("肥満度検査結果（グループ会社）：" + resultDi);
+		System.out.println("肥満度検査結果（DI導入後）：" + resultDi);
 	}
 }
