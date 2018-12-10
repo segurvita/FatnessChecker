e6cbd6e04857778fc49827b89b25b5fadc90d131
segur <segur.opus@gmail.com>
2018-12-10 10:30:31 +0900

    Add tests of 5 level of FatnessChecker

--- src/test/java/com/segur/fatness/FatnessCheckerTest.java
+++ src/test/java/com/segur/fatness/FatnessCheckerTest.java
@@ -27,13 +27,78 @@ class FatnessCheckerTest {
 	 * BMI判定のテスト
 	 */
 	@Test
-	void testCheck() {
+	void test低体重() {
 
 		// BMI計算する。
-		String result = this.fatnessChecker.check(170.0, 57.8);
+		String result = this.fatnessChecker.check(170.0, 17.0);
 
 		// 計算結果を判定する。
-		assertThat(result).isEqualTo("肥満（２度）");
+		assertThat(result).isEqualTo("低体重");
+	}
+
+	/**
+	 * BMI判定のテスト
+	 */
+	@Test
+	void test標準体重() {
+
+		// BMI計算する。
+		String result = this.fatnessChecker.check(170.0, 31.45);
+
+		// 計算結果を判定する。
+		assertThat(result).isEqualTo("普通体重");
+	}
+
+	/**
+	 * BMI判定のテスト
+	 */
+	@Test
+	void test１度肥満() {
+
+		// BMI計算する。
+		String result = this.fatnessChecker.check(170.0, 42.5);
+
+		// 計算結果を判定する。
+		assertThat(result).isEqualTo("１度肥満");
+	}
+
+	/**
+	 * BMI判定のテスト
+	 */
+	@Test
+	void test２度肥満() {
+
+		// BMI計算する。
+		String result = this.fatnessChecker.check(170.0, 51.0);
+
+		// 計算結果を判定する。
+		assertThat(result).isEqualTo("２度肥満");
+	}
+
+	/**
+	 * BMI判定のテスト
+	 */
+	@Test
+	void test３度肥満() {
+
+		// BMI計算する。
+		String result = this.fatnessChecker.check(170.0, 59.5);
+
+		// 計算結果を判定する。
+		assertThat(result).isEqualTo("３度肥満");
+	}
+
+	/**
+	 * BMI判定のテスト
+	 */
+	@Test
+	void test４度肥満() {
+
+		// BMI計算する。
+		String result = this.fatnessChecker.check(170.0, 68.0);
+
+		// 計算結果を判定する。
+		assertThat(result).isEqualTo("４度肥満");
 	}
 
 }
